a4638fee2ffcaeed9416fa912f08653f0c0195f6
Nurcholis Koem <nurcholis.a.koem@gmail.com>
2023-12-29 19:03:22 +0700

    update

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -46,7 +46,7 @@ class ProductController extends Controller
             'stock' => (int) $request->stock,
             'category' => $request->category,
             'image' => $filename,
-            'is_favorite' => $request->is_favorite
+            'is_best_seller' => $request->is_best_seller
         ]);
 
         if ($product) {
